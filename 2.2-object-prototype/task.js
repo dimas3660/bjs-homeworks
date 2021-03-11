2eb65eba11b7cb60bed2e6daba25e0e5444e46df
Gorbachyov Dmitriy <dimas36601@yandex.ru>
2021-03-11 20:31:23 +0300

    Задача 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,37 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function isPalindrome () {
+    let str = this.toLowerCase();
+    let string1 = str.split(" ").join("");
+    let string2 = string1.split("").reverse().join("");
+    return string1 === string2;
+}
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
-    // return averageMark
+    let numberRatings = marks.length;
+    let average = 0;
+
+    if (numberRatings == 0) {
+        return 0;
+    }
+
+    for (let i = 0; i < marks.length; i++) {
+        average += marks[i];
+    }
+
+    average /= marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
-    // return verdict
+    let now = (new Date).getTime();
+    let date = Date.parse(birthday);
+    let diff = Math.round((now - date)/ (1000*3600*24));
+    let year = (30.4375)*12;
+    let age = Math.floor(diff/year);
+    if (age>=18) {
+        return true;
+    }
+   
 }
